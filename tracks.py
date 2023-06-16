03022285c21150272ea838fae8719f580912050d
Cecilia Acosta <ceciliaacosta78@hotmail.com>
2023-06-16 18:08:15 -0400

    script for fast tracks

--- tracks.py
+++ tracks.py
@@ -1,6 +1,7 @@
-g
+
 import os
 import pandas as pd
+
 import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -10,7 +11,8 @@ import sys, os
 import math
 import re
 
-pixel=int(input("Enter pixel size in nanometer:"))
+
+#pixel=int(input("Enter pixel size in nanometer:"))
 analysis= input(("Enter B if you want to analyse TracksBound only. Enter L if you want to analyse TracksLifetime only. Enter BL if you want to analyse BOTH:"))
 rows_to_add=[]
 header=["Cell_ID","Track_IDs", "spt_tr", "spt_widt", "mean_sp", "max_sp", "min_sp", "med_sp", "std_sp", "mean_q", "max_q_tr", "min_q_tr", "med_q_tr", "std_q_tr", "tr_dur", "tr_start", "tr_fin", "x_lc", "y_lc"]
@@ -43,21 +45,23 @@ def point_in_poly(x, y, poly):
 
 def fn(outlines, extracted_data):
     extracted_df = pd.DataFrame(extracted_data)
-    print(extracted_df)
+    #print(extracted_df)
     for i in range (len(outlines)):
         
         poly=outlines[i]
+        print(i)
       
         for j in range ( len(extracted_data)):
 #MAKE SURE TO FIRST OPEN THE RESULTS DATA TO SEE HOW THE COLUMNS WERE SAVED (sometimes FIJI saves the results with one extra empty column at the beginning)
-            x=extracted_data[j][16]/(pixel/1000)  #CHANGE THIS SO THE COLUMN IS ALIGNED WITH X_(px)
-            y=extracted_data[j][17]/(pixel/1000)    #CHANGE THIS SO THE COLUMN IS ALIGNED WITH Y_(px)
-     
+            x=extracted_data[j][16] #CHANGE THIS SO THE COLUMN IS ALIGNED WITH X_(px)
+            y=extracted_data[j][17]    #CHANGE THIS SO THE COLUMN IS ALIGNED WITH Y_(px)
+            #print(x, y)
             if point_in_poly(x,y,poly): 
+                #print("inside")
        
                 row_data = extracted_df.iloc[j].values
-                new_list=[i]+row_data
-                rows_to_add.append(new_list)
+                row_data= np.insert(row_data, 0, i )
+                rows_to_add.append(row_data)
     return rows_to_add
                     
 def main(folderTrack, folderSeg):
@@ -66,11 +70,17 @@ def main(folderTrack, folderSeg):
     boS=False
     files=[]
     j=0
-    
+
+
     for filename in sorted(os.listdir(folderSeg)):
         f = os.path.join(folderSeg, filename)
         if not filename.endswith(".DS_Store"):
             files.append(f)
+    print(files)
+    seg_file = files[j]
+    seg_masks=np.load(seg_file, allow_pickle=True).item()
+    outlines= utils.outlines_list(seg_masks['masks'])
+
 
     for filename in sorted(os.listdir(folderTrack)):
         if filename.endswith("DS.store"):
@@ -80,65 +90,61 @@ def main(folderTrack, folderSeg):
 
             tr= os.path.join(folderTrack, filename)
             tracksB= np.genfromtxt(tr, delimiter=',', skip_header=0)
+      
             f1= filename
             boB=True
-            file_pathB = os.path.join(folderTrack, "trim"+filename+".csv")
+            file_pathB = os.path.join(folderTrack, "trim2"+filename)
 
-        elif filename.endswith(".npy"):
-
-            seg_file = files[i]
-            seg_masks=np.load(seg_file, allow_pickle=True).item()
-
-            # masks=seg_masks['masks']
-            outlines= utils.outlines_list(seg_masks['masks'])
-            #print(outlines)
-            n=len(outlines)
-            boS=True
             
         elif (filename.endswith("tracksDiffusive.csv")):
 
             tr= os.path.join(folderTrack, filename)
             tracksL= np.genfromtxt(tr, delimiter=',', skip_header=0)
+   
             f2=filename
             boL=True
-            file_pathL = os.path.join(folderTrack, "trim"+filename+".csv")
+            file_pathL = os.path.join(folderTrack, "trim2"+filename)
         
-        elif (boB and boS and analysis=="B"):
+        elif (boB and analysis=="B"):
             j+=1 
             dfB = pd.DataFrame(fn(outlines, tracksB))
-            dfB.columns=header
-            dfB.to_csv(file_pathB, index=True, header=True, sep=',')
+
+            dfB.to_csv(file_pathB, index=False, header=False, sep=',')
 
             boB=False
             boS=False
  
-        elif (boL and boS and analysis=="L"):
+        elif (boL and analysis=="L"):
             j+=1
             dfL = pd.DataFrame(fn(outlines, tracksL))
-            dfL.columns=header
-            dfL.to_csv(file_pathL, index=True, header=True, sep=',')
+            #dfL.columns=header
+            dfL.to_csv(file_pathL, index=False, header=False, sep=',')
 
             boS=False
             boL=False
 
-        elif (boB and boS and boL and analysis=="BL"):
+        elif (boB and boL and analysis=="BL"):
+            print("here 3 ")
             j+=1
-            f1 = re.sub('tracksBound$', '', f1)
-            f2= re.sub('tracksLifetime$', '', f2)
-
+            f1 = re.sub('tracksBound.csv$', '', f1)
+            f2= re.sub('tracksDiffusive.csv$', '', f2)
+            print(f1, f2)
             if f1==f2: 
+            
 
                 dfB = pd.DataFrame(fn(outlines, tracksB))
+       
                 dfL = pd.DataFrame(fn(outlines, tracksL))
+         
 
-                dfB.columns=header
-                dfL.columns=header
+                #dfB.columns=header
+                #dfL.columns=header
 
-                dfB.to_csv(file_pathB, index=True, header=True, sep=',')
-                dfL.to_csv(file_pathL, index=True, header=True, sep=',')
+                dfB.to_csv(file_pathB, index=True, header=False, sep='\t')
+                dfL.to_csv(file_pathL, index=True, header=False, sep='\t')
 
                 boB=False
-                boS=False
+       
                 boL=False
             else: 
                 continue 
